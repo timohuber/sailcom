da712fbbb3bf373d2ea99d5b2c14b533e6aae3e9
Timo <timo.huber@bluemail.ch>
2020-07-13 14:42:25 +0200

    added title

--- backend/app/lake/models.py
+++ backend/app/lake/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
 class Lake(models.Model):
+    title = models.CharField(max_length=100)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
