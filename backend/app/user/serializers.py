239c68640b0b368ab02c05d3a32705b4dac1ee29
Andreas Hirt <athirt@gmail.com>
2020-07-14 15:28:53 +0200

    added events

--- backend/app/user/serializers.py
+++ backend/app/user/serializers.py
@@ -7,4 +7,5 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name']
+        fields = ['id', 'email', 'first_name', 'last_name', 'street', 'address_appendix', 'city', 'zip_code', 'phone',
+                  'mobile', 'date_of_birth', 'licence', 'joined', 'avatar', 'licence_ok', 'entry_fee_paid']
