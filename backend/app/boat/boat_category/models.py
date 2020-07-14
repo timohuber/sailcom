3a66dc1ca3893e95ba2649b08943ded929a6ea8b
Andreas Hirt <athirt@gmail.com>
2020-07-14 11:55:06 +0200

    boatendpointsfjdsakfjksdalj

--- backend/app/boat/boat_category/models.py
+++ backend/app/boat/boat_category/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class BoatCategory(models.Model):
+    category_name = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.category_name}'
--- backend/app/boat/serializers.py
+++ backend/app/boat/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 from .models import Boat
+from ..boat_crew.serializers import BoatCrewSerializer
 
 
 class BoatSerializer(serializers.ModelSerializer):
+    crew = BoatCrewSerializer(read_only=True)
+
     class Meta:
         model = Boat
         fields = ['id', 'title', 'price_weekday', 'price_weekend', 'price_full_day', 'description',
                   'a_license_required', 'length', 'width', 'draught', 'max_crew', 'recommended_crew', 'mooring',
-                  'type', 'crew']
+                  'model', 'crew']
