0eca2d59d9715d354abb98bc0fd5dbe04c3abcfb
Andreas Hirt <athirt@gmail.com>
2020-07-14 13:56:53 +0200

    boat views

--- backend/app/boat/serializers.py
+++ backend/app/boat/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
+from .image.serializers import ImageSerializer
 from .models import Boat
 from ..boat_crew.serializers import BoatCrewSerializer
 
 
 class BoatSerializer(serializers.ModelSerializer):
     crew = BoatCrewSerializer(read_only=True)
+    images = ImageSerializer(read_only=True, many=True)
 
     class Meta:
         model = Boat
         fields = ['id', 'title', 'price_weekday', 'price_weekend', 'price_full_day', 'description',
                   'a_license_required', 'length', 'width', 'draught', 'max_crew', 'recommended_crew', 'mooring',
-                  'model', 'crew']
+                  'model', 'crew', 'images']
