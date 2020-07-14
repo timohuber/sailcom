5961e70b8a6ffc2c0db132c8e0ee91d79c110815
Timo <timo.huber@bluemail.ch>
2020-07-14 18:59:58 +0200

    another fix

--- backend/app/boat/detailserializer.py
+++ backend/app/boat/detailserializer.py
@@ -17,6 +17,7 @@ class DetailBoatSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Boat
-        fields = ['id', 'title', 'price_weekday', 'price_weekend', 'price_full_day', 'description',
+        fields = ['id', 'title', 'price_hour_weekday', 'price_hour_weekend', 'price_fullday_weekday',
+                  'price_fullday_weekend', 'price_full_day', 'description',
                   'a_license_required', 'length', 'width', 'draught', 'max_crew', 'recommended_crew', 'mooring',
                   'model', 'crew', 'images', 'boat_documents', 'bookings']
