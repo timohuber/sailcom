3e8166e5490277dd9e43e82e6ee4c4002084db54
Andreas Hirt <athirt@gmail.com>
2020-07-15 18:45:54 +0200

    added instructor in event

--- backend/app/event/serializers.py
+++ backend/app/event/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from .models import Event
+from ..user.serializers import UserSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
+    instructor = UserSerializer(read_only=True, many=True)
     class Meta:
         model = Event
-        exclude = ()
+        fields = ['id', 'title', 'price', 'created', 'updated',
+                  'from_date_time', 'until_date_time', 'meeting_point',
+                  'boat_model', 'event_type', 'instructor', 'participants']
