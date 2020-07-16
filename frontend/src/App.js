f48073176585336d3c8907ae6a0620235c33ee2a
ramon_koestli <ramonkos@gmail.com>
2020-07-16 17:43:57 +0200

    create boat-sharing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import {useDispatch} from "react-redux";
-import {fetchUserData} from "./store/actions/loginActions";
+import { useDispatch } from 'react-redux';
+import { fetchUserData } from './store/actions/loginActions';
 
 import './css/main.css';
 
@@ -17,15 +17,15 @@ import RegistrationPage from './pages/register';
 import BoatsListPage from './pages/boatslistpage';
 import VerificationPage from './pages/verification';
 import MyProfilePage from './pages/my-profile';
-import BoatDetailPage from './pages/boatdetails'
-import EventPage from './pages/events'
+import BoatDetailPage from './pages/boatdetails';
+import EventPage from './pages/events';
+import BoatSharing from './pages/boat-sharing';
 
 // remove
 import Components from './pages/components';
 
 function App() {
-
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     if (localStorage.getItem('accessToken')) {
         dispatch(fetchUserData());
     }
@@ -34,22 +34,31 @@ function App() {
         <div className='App'>
             <BrowserRouter>
                 <Header />
-                    <main>
-                        <Route exact path='/'>
-                            <Redirect to='/intro' />
-                        </Route>
-                        <Route exact path='/home' component={Home} />
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/registrierung' component={RegistrationPage} />
-                        <Route exact path='/bootsliste' component={BoatsListPage} />
-                        <Route exact path='/boot/:id' component={BoatDetailPage} />
-                        <Route exact path='/verifikation' component={VerificationPage} />
-                        <Route exact path='/intro' component={Intro} />
-                        <Route exact path='/profil' component={MyProfilePage} />
-                        <Route exact path='/events' component={EventPage} />
+                <main>
+                    <Route exact path='/'>
+                        <Redirect to='/intro' />
+                    </Route>
+                    <Route exact path='/home' component={Home} />
+                    <Route exact path='/boat-sharing' component={BoatSharing} />
+                    <Route exact path='/login' component={Login} />
+                    <Route
+                        exact
+                        path='/registrierung'
+                        component={RegistrationPage}
+                    />
+                    <Route exact path='/bootsliste' component={BoatsListPage} />
+                    <Route exact path='/boot/:id' component={BoatDetailPage} />
+                    <Route
+                        exact
+                        path='/verifikation'
+                        component={VerificationPage}
+                    />
+                    <Route exact path='/intro' component={Intro} />
+                    <Route exact path='/profil' component={MyProfilePage} />
+                    <Route exact path='/events' component={EventPage} />
 
-                        <Route exact path='/components' component={Components} />
-                    </main>
+                    <Route exact path='/components' component={Components} />
+                </main>
                 <Footer />
             </BrowserRouter>
         </div>
