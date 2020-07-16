82051b87c13a5460697526ee97c0bde833d03865
Andreas Hirt <athirt@gmail.com>
2020-07-16 12:28:37 +0200

    event re- and ungistration

--- backend/app/event/urls.py
+++ backend/app/event/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 
 from .event_type.views import ListEventTypeView
 
-from .views import ListEventView, ListEventsView, ListEventsAllView
+from .views import ListEventView, ListEventsView, ListEventsAllView, RegisterEventView
 
 urlpatterns = [
     path('', ListEventsView.as_view()),
     path('all/', ListEventsAllView.as_view()),
     path('<int:pk>/', ListEventView.as_view()),
     path('type/', ListEventTypeView.as_view()),
+    path('register/<int:pk>/', RegisterEventView.as_view())
 ]
