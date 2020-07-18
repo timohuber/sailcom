b0bc404ad1c90d86474c4e6f409df1a8f56cdf1f
ramon_koestli <ramonkos@gmail.com>
2020-07-18 13:32:21 +0200

    removed unnecessary code

--- frontend/src/store/constants.js
+++ frontend/src/store/constants.js
@@ -30,7 +30,7 @@ export const countrySelection = [
 ];
 
 export const eventType = [
-    { key: 'einweisungen', value: 'Einweisungen' },
-    { key: 'mitsegeln', value: 'Mitsegeln' },
-    { key: 'sailingladies', value: 'SailingLadies' },
+    { key: 1, value: 'Einweisungen' },
+    { key: 2, value: 'Mitsegeln' },
+    { key: 3, value: 'SailingLadies' },
 ];
