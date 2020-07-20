cfd34cdaf2d66fcdc7311c171168581aed06392a
Andreas Hirt <athirt@gmail.com>
2020-07-20 18:17:33 +0200

    updated boat endpoint for search functionality

--- backend/app/boat/urls.py
+++ backend/app/boat/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import ListBoatsView, ListBoatView, ListBoatsWhereCrewView, SearchBoatsView, FavouriteTodayView, \
-    SearchBoatsView2
+from .views import ListBoatsView, ListBoatView, ListBoatsWhereCrewView, SearchBoatsView, FavouriteTodayView
 from .boat_model.views import ListBoatModel
 from ..boat_crew.views import ListBoatCrewView, ListBoatCrewMemberView
 from .boat_category.views import ListBoatCategories
@@ -9,7 +8,6 @@ from .boat_category.views import ListBoatCategories
 urlpatterns = [
     path('', ListBoatsView.as_view()),
     path('search/', SearchBoatsView.as_view()),
-    path('search2/', SearchBoatsView2.as_view()),
     path('wherecrew/', ListBoatsWhereCrewView.as_view()),
     path('<int:pk>/', ListBoatView.as_view()),
     path('models/', ListBoatModel.as_view()),
