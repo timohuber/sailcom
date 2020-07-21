5a8c3ed693d399b095da7fb6c7fa05b2a5a9f790
Andreas Hirt <athirt@gmail.com>
2020-07-21 11:50:07 +0200

    is_crew and instructed_for_models read only

--- backend/app/permissions.py
+++ backend/app/permissions.py
@@ -45,7 +45,7 @@ class UserIsCurrentUser(BasePermission):
         if request.method == 'GET':
             return True
         return obj.author == request.user
-        
+
 class IsStaff(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == 'GET':
