c0ed667c64b0e107a6a82f4209b08e6c4021f6f7
ramon_koestli <ramonkos@gmail.com>
2020-07-21 10:34:52 +0200

    solved merge conflicts again

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,25 +1,15 @@
 import {combineReducers} from 'redux';
 import {currentUser} from './currentUser';
-<<<<<<< HEAD
 import { events } from './events';
 import {boatInfo} from './boatReducer';
-||||||| 804000f
-import {events} from './events';
-=======
 import {events} from './events';
 import {boats} from './boat';
 
->>>>>>> master
 
 export const reducers = combineReducers({
     currentUser,
-<<<<<<< HEAD
     events,
     boatInfo,
-||||||| 804000f
-    events
-=======
     events,
     boats
->>>>>>> master
 })
